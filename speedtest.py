e4c2cd8f27987388896886128a37187cd837cbc7
Aaron Grisez <acgrisez@gmail.com>
2021-01-02 13:55:56 -0800

    Adding system metrics

--- speedtest.py
+++ speedtest.py
@@ -3,6 +3,9 @@ import schedule
 import time
 import re
 import subprocess
+import socket
+import shutil
+import psutil
 from influxdb import InfluxDBClient
 
 INFLUX_DB_HOST = os.environ.get("NETWATCH_INFLUX_DB_HOST", None)
@@ -15,7 +18,7 @@ if INFLUX_DB_HOST is None:
 
 client = InfluxDBClient(INFLUX_DB_HOST, 8086, 'speedmonitor', INFLUX_DB_PASS, 'internetspeed')
 
-def record():
+def record_speed():
     response = subprocess.Popen('speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
     ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
     download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
@@ -28,6 +31,9 @@ def record():
     speed_data = [
         {
             "measurement" : "internet_speed",
+            "tags": {
+                "host": socket.gethostname()
+            },
             "fields" : {
                 "download": float(download),
                 "upload": float(upload),
@@ -37,7 +43,26 @@ def record():
     ]
     client.write_points(speed_data)
 
-schedule.every(3).seconds.do(record)
+def record_system_data():
+    total, used, free = shutil.disk_usage("/")
+    data = [
+        {
+            "measurement" : "system_data",
+            "tags": {
+                "host": socket.gethostname()
+            },
+            "fields" : {
+                "percent_used_disk_space": float(round(used / total * 100.0, 2)),
+                "percent_used_ram":  float(psutil.virtual_memory().percent),
+                "percent_used_cpu": float(psutil.cpu_percent())
+            }
+        }
+    ]
+    print(data)
+    client.write_points(data)
+
+schedule.every(1).minute.do(record_speed)
+schedule.every(1).second.do(record_system_data)
 
 if __name__ == "__main__":
     while True:
